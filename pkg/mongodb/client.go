f22bf8f2ab34977a9d472c3ac3f2dd8703bdb898
soloden <akyztick@yandex.ru>
2021-09-28 09:46:14 +0300

    Set username and password not required

--- pkg/mongodb/client.go
+++ pkg/mongodb/client.go
@@ -10,17 +10,30 @@ import (
 )
 
 func NewClient(ctx context.Context, username string, password string, host string, port string, database string) (*mongo.Database, error) {
-	mongoDBURL := fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
-	credentials := options.Credential{
-		Username:    username,
-		Password:    password,
-		PasswordSet: true,
+	var mongoDBURL string
+	var anonymous bool
+
+	if username != "" || password != "" {
+		mongoDBURL = fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
+	} else {
+		anonymous = true
+		mongoDBURL = fmt.Sprintf("mongodb://%s:%s", host, port)
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoDBURL)
+
+	if !anonymous {
+		clientOptions.SetAuth(options.Credential{
+			Username:    username,
+			Password:    password,
+			PasswordSet: true,
+		})
 	}
 
 	reqCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(reqCtx, options.Client().ApplyURI(mongoDBURL).SetAuth(credentials))
+	client, err := mongo.Connect(reqCtx, clientOptions)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create client to mongodb due to error %w", err)
 	}
